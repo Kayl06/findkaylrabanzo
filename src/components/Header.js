f800620d28809f93a3f8a870ce74930ef7692e3d
Kayl Rabanzo <frabanzoo@gmail.com>
2023-04-28 00:34:45 +0800

    added hamburger nav with animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,10 +33,9 @@ export default function Header() {
       <Link
         key={index}
         href={menuItem.path}
-        className="__menut-items hover:text-[#fff] hover:bg-[#373737b4] transition ease-in-out delay-100 hover:-translate-y-1 motion-reduce:transition motion-reduce:hover:transform flex items-center gap-2 p-5 md:p-1 px-2 rounded"
+        className="__menut-items hover:text-[#fff] hover:bg-[#373737b4] transition ease-in-out delay-100 hover:-translate-y-1 motion-reduce:transition motion-reduce:hover:transform flex items-center justify-center gap-2 p-5 md:p-1 px-2 rounded"
       >
-        <span className="text-[#efefef]">0{index + 1}.</span>
-        {menuItem.name}
+        <span className="text-[#efefef]"> 0{index + 1}. </span> {menuItem.name}
       </Link>
     );
   });
@@ -56,10 +55,13 @@ export default function Header() {
     };
   }, []);
 
-  let parentMenuClassName = "";
+  let parentMenuClassName = "hidden lg:flex";
   let childMenuClassName = "";
+
   if (isMobile) {
-    parentMenuClassName = isMenuOpen && `fixed inset-0`;
+    parentMenuClassName = isMenuOpen
+      ? "fixed opacity-100 visible "
+      : "invisible opacity-0";
     childMenuClassName =
       isMenuOpen &&
       `p-10 top-[80px] right-0 flex-col fixed flex bg-black h-screen w-full`;
@@ -68,21 +70,20 @@ export default function Header() {
   return (
     <>
       <Head>
-        <title>Fedimar Kayl Rabanzo &#8212; Full Stack Web Developer</title>
+        <title> Fedimar Kayl Rabanzo & #8212; Full Stack Web Developer</title>
+
         <link rel="shortcut icon" href="/images/profile1.png" />
       </Head>
-
       <div className=" z-[11] __header fixed md:relative w-full py-2 items-center justify-between text-sm flex text-gray-400 font-light lg:px-[50px] px-[20px]">
         <Logo />
 
         <div
-          className={`__menu font-mono ${parentMenuClassName} hidden md:flex`}
+          className={`__menu font-mono transition duration-300 ${parentMenuClassName}`}
         >
           <div
             className={`__menu_inner gap-[20px] md:flex ${childMenuClassName}`}
           >
             {renderedMenuList}
-
             <a
               href="/files/CV - FEDIMAR KAYL RABANZO.pdf"
               target="_blank"
@@ -92,13 +93,12 @@ export default function Header() {
             </a>
           </div>
         </div>
-
         <div
-          className="__hamburger flex md:hidden cursor-pointer"
+          className={`${isMenuOpen ? "__open" : "__close"} __hamburger flex lg:hidden cursor-pointer`}
           onClick={handleBurgerMenuClick}
         >
           <div className="__hamburger_box">
-            <div className="__hamburger_box_inner"></div>
+            <div className="__hamburger_box_inner"> </div>
           </div>
         </div>
       </div>
